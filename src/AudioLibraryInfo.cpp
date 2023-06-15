ca364935a86fa99e5f517219448232e9d476a49a
Zlolka <jaroslavderkach@gmail.com>
2023-06-15 22:57:55 +0300

    feat: Release AudioLibraryInfo::getOurAudioLibraryInfo()

--- src/AudioLibraryInfo.cpp
+++ src/AudioLibraryInfo.cpp
@@ -1,4 +1,8 @@
+#include <cassert>
+#include <fstream>
+
 #include "AudioLibraryInfo.hpp"
+#include "Settings.hpp"
 
 namespace AudioSync {
 
@@ -12,6 +16,27 @@ const decltype(AudioLibraryInfo::file_name)& AudioLibraryInfo::getFileName() con
 
 const decltype(AudioLibraryInfo::childs)& AudioLibraryInfo::getChilds() const {
     return childs;
+}
+
+AudioLibraryInfo AudioLibraryInfo::getOurAudioLibraryInfo() {
+    const auto settings = Settings::getSettings();
+    const auto storageFileName = settings->getStorageFileName();
+
+    std::filebuf fileBuf;
+    if (not fileBuf.open(storageFileName, std::ios::in)) { // TODO: Rewrite as work in files
+        assert(!"Storage file not open");
+    }
+    const auto fileSize = fileBuf.in_avail();
+    std::string strFromFile(fileSize, '\0');
+    const std::size_t countReadedChars = fileBuf.sgetn(
+        const_cast<char *>(strFromFile.c_str())
+        , fileSize
+    );
+    if (strFromFile.size() != countReadedChars) {
+        assert(!"Storage file not readed");
+    }
+
+    return deserialize(strFromFile);
 }
 
 }
