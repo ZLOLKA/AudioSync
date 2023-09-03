5063f1fbead2a07eefd17657ff1b1b2ec3f152dc
Zlolka <jaroslavderkach@gmail.com>
2023-09-03 15:05:14 +0300

    test: feat: Create C-tor and D-tor for NormalSettingsFile

---
+++ tests/cpp_tests/src/NormalSettingsFile.cpp
@@ -0,0 +1,36 @@
+#include "NormalSettingsFile.hpp"
+
+#include "DebugTools/ASSERT.hpp"
+#include "Settings.hpp"
+
+#include <yaml-cpp/yaml.h>
+
+#include <filesystem>
+#include <fstream>
+
+namespace AudioSync::Tests {
+
+NormalSettingsFile::NormalSettingsFile() {
+  std::ofstream settingsFile(::AudioSync::Settings::fileName);
+  ASSERT(settingsFile.is_open(), "SettingsFile has not been created");
+
+  ::YAML::Emitter yaml(settingsFile);
+  yaml << ::YAML::BeginMap;
+  yaml << ::YAML::Key << "storageFileName" << ::YAML::Value << "AudioSync.yaml";
+  yaml << ::YAML::Key << "path2RootDir" << ::YAML::Value << "./Music";
+  yaml << ::YAML::Key << "path2OtherDirectories" << ::YAML::Value
+       << ::YAML::BeginSeq << ::YAML::EndSeq;
+  yaml << ::YAML::Key << "streamingServicesNames" << ::YAML::Value
+       << ::YAML::BeginSeq << ::YAML::EndSeq;
+  yaml << ::YAML::EndMap;
+}
+
+NormalSettingsFile::~NormalSettingsFile() {
+  try {
+    std::filesystem::remove(::AudioSync::Settings::fileName);
+  } catch (std::filesystem::filesystem_error& e) {
+    ASSERT(false, "SettingsFile has not been deleted");
+  }
+}
+
+}  // namespace AudioSync::Tests
